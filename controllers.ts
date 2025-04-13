4750b475366f6b47c6fecae656725ca65dfc33b0
tbmeans <tbmeans2005@gmail.com>
2025-04-13 00:03:11 -0400

    mult. ctrls not 1-size-fits-all

--- controllers.ts
+++ controllers.ts
@@ -3,6 +3,20 @@
 import models from "./models";
 import views from "./views";
 
-export default function controllers(routeName: string, token: string) {
-	return views.render(models.query(routeName, token));
-}
+const index = (routeName: string, errorMessage: string = ''): string => {
+	return views.index.render(models.query(routeName, '', errorMessage));
+};
+
+const login = (routeName: string, token: string): string => {
+	return views.login.render(models.query(routeName, token));
+};
+
+const auth = (routeName: string): string => {
+	return views.auth.render(models.query(routeName));
+};
+
+const app1 = (routeName: string): string => {
+	return views.app1.render(models.query(routeName));
+};
+
+export default { index, login, auth, app1, }
